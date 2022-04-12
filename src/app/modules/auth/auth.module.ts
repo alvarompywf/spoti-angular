a0d5d10d728eb98a8ca57fb4bf27134bf1fad3df
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-04-12 10:18:15 +0200

    feat: routing, lazy-loading

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { AutPagesComponent } from './pages/aut-pages/aut-pages.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AutPagesComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
